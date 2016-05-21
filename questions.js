59870bd221ee3810fb903ed7c2ac2c50dc7e7206
Julia Oran <jlkoran@users.noreply.github.com>
2016-05-21 12:05:25 -0500

    stick_results2

--- questions.js
+++ questions.js
@@ -4,3 +4,28 @@ $("input[name='age']").click(function () {
     $('#ya-form').css('display', ($(this).val() === 'ya') ? 'block':'none');
     $('#adult-form').css('display', ($(this).val() === 'adult') ? 'block':'none');
 });
+
+// function f1(){
+//     // $("input[id='theme']").click(function () {
+//     // 	$('#sophisticated')
+//     // }
+
+// 	document.getElementById("function-click").href="stock_results2.html";
+// }
+
+// $("input[id='theme']").click(function () {
+//     $('#sophisticated').click(function() {
+// 			  window.location='/stock_results.html';
+// 			});
+//     $('#superheroes').click(function() {
+// 			  window.location='stock_results2.html';
+// 			});
+// });
+
+
+// function submit(){
+
+// }
+
+// myLink = document.getElementById('function-click');
+// myLink.onclick = submit();
